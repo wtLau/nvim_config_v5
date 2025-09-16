8284101dc4980c10123a699c59d7dcab5e3714bf
Brian Lau <brian.lau.external@arcteryx.com>
2025-09-16 11:39:09 -0700

    chore: remove gf mapping for v3 update
    df
    
    df

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,4 +1,3 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "obsidian-nvim/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
@@ -7,28 +6,6 @@ return {
   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand':
   -- event = { "bufreadpre " .. vim.fn.expand "~" .. "/wtlau-obsidian/**.md" },
   -- event = { "BufReadPre  */wtlau-obsidian/*.md" },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    {
-      "AstroNvim/astrocore",
-      opts = {
-        mappings = {
-          n = {
-            ["gf"] = {
-              function()
-                if require("obsidian").util.cursor_on_markdown_link() then
-                  return "<Cmd>ObsidianFollowLink<CR>"
-                else
-                  return "gf"
-                end
-              end,
-              desc = "Obsidian Follow Link",
-            },
-          },
-        },
-      },
-    },
-  },
   opts = {
     workspaces = {
       {
@@ -72,10 +49,7 @@ return {
       blink = true,
     },
 
-    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
-    -- URL it will be ignored but you can customize this behavior here.
-    follow_url_func = vim.ui.open or function(url) require("astrocore").system_open(url) end,
-
+    preferred_link_style = "wiki",
     -- Optional, customize how markdown links are formatted.
     markdown_link_func = function(opts) return require("obsidian.util").markdown_link(opts) end,
 
@@ -83,15 +57,13 @@ return {
     ---@param title string|?
     ---@return string
     note_id_func = function(title)
-      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-      -- In this case a note with the title 'My new note' will be given an ID that looks
-      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      -- E.g. like 'my-new-note-1657296016'
       local prefix = ""
       if title ~= nil then
         -- If title is given, transform it into valid file name.
         prefix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
       else
-        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        -- If prefix is nil, just add 4 random uppercase letters to the suffix.
         for _ = 1, 4 do
           prefix = prefix .. string.char(math.random(65, 90))
         end
@@ -104,9 +76,7 @@ return {
     note_frontmatter_func = function(note)
       -- Add the title of the note as an alias.
       if note.title then note:add_alias(note.title) end
-
       local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-
       -- `note.metadata` contains any manually added fields in the frontmatter.
       -- So here we just make sure those fields are kept in the frontmatter.
       if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
@@ -114,7 +84,6 @@ return {
           out[k] = v
         end
       end
-
       return out
     end,
   },
