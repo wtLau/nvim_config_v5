8c800c54ee81bd76909bac8cd3eb9f2e92f82197
Brian Lau <brian.lau.external@arcteryx.com>
2025-09-16 11:50:04 -0700

    chore: clean up for v3

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,11 +1,10 @@
 return {
   "obsidian-nvim/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
+  version = "*", --  use latest release 
   lazy = true,
   ft = "markdown",
-  -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand':
-  -- event = { "bufreadpre " .. vim.fn.expand "~" .. "/wtlau-obsidian/**.md" },
-  -- event = { "BufReadPre  */wtlau-obsidian/*.md" },
+  ---@module 'obsidian'
+  ---@type obsidian.config
   opts = {
     workspaces = {
       {
@@ -17,41 +16,25 @@ return {
         path = "~/Documents/wtlau-obsidian/notes/work",
       },
     },
-    dir = vim.env.HOME .. "/Documents/wtlau-obsidian", -- specify the vault location. no need to call 'vim.fn.expand' here
-
+    dir = vim.env.HOME .. "/Documents/wtlau-obsidian", -- vault location
     templates = {
       subdir = "templates",
       date_format = "%Y-%m-%d-%a",
       time_format = "%H:%M",
     },
-
-    -- Optional, if you keep notes in a specific subdirectory of your vault.
     notes_subdir = "notes",
-
     daily_notes = {
-      -- Optional, if you keep daily notes in a separate directory.
       folder = "daily",
-      -- Optional, if you want to change the date format for the ID of daily notes.
       date_format = "%Y-%m-%d",
-      -- Optional, if you want to change the date format of the default alias of daily notes.
       alias_format = "%B %-d, %Y",
-      -- Optional, default tags to add to each new daily note created.
       default_tags = { "daily-notes" },
-      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
       template = "daily.md",
     },
-
-    -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
     completion = {
-      -- Enables completion using nvim_cmp
       nvim_cmp = false,
-      -- Enables completion using blink.cmp
       blink = true,
     },
-
     preferred_link_style = "wiki",
-    -- Optional, customize how markdown links are formatted.
-    markdown_link_func = function(opts) return require("obsidian.util").markdown_link(opts) end,
 
     -- Optional, customize how note IDs are generated given an optional title.
     ---@param title string|?
