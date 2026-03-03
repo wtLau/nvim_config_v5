ba2921bc5e37061adb55d4716eef872bc4508e11
Brian Lau <brian.lau.external@arcteryx.com>
2026-03-03 10:45:33 -0800

    Lock obsidian.nvim to 3.14.8

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,7 +1,8 @@
 return {
   "obsidian-nvim/obsidian.nvim",
-  version = "*", --  use latest release
-  -- version = "3.15.10", --  2026-03-03
+  -- version = "*", --  use latest release
+  version = "3.14.8", --  Dec 17, 2025
+
   lazy = true,
   ft = "markdown",
   ---@module 'obsidian'
@@ -70,6 +71,22 @@ return {
       return prefix .. "-" .. tostring(os.time())
     end,
 
+    -- Pre   version = "3.14.8"
+    note_frontmatter_func = function(note)
+      -- Add the title of the note as an alias.
+      if note.title then note:add_alias(note.title) end
+      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+      return out
+    end,
+
+    -- Post version = "3.14.8"
     -- customize the frontmatter data.
     frontmatter = {
       enabled = true,
