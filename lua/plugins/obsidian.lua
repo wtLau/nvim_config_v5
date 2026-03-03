4e3e7fe0c61f6f30e37f169d7132062ae7272345
Brian Lau <brian.lau.external@arcteryx.com>
2026-03-03 10:02:29 -0800

    chore: obsidian- add template folder

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,6 +1,7 @@
 return {
   "obsidian-nvim/obsidian.nvim",
   version = "*", --  use latest release
+  -- version = "3.15.10", --  2026-03-03
   lazy = true,
   ft = "markdown",
   ---@module 'obsidian'
@@ -17,10 +18,11 @@ return {
       },
     },
     dir = vim.env.HOME .. "/Documents/wtlau-obsidian", -- vault location
+    note = {
+      template = "default.md",
+    },
     templates = {
-      subdir = "templates",
-      date_format = "%Y-%m-%d-%a",
-      time_format = "%H:%M",
+      folder = "/templates",
     },
     notes_subdir = "notes",
     daily_notes = {
@@ -68,20 +70,35 @@ return {
       return prefix .. "-" .. tostring(os.time())
     end,
 
-    -- Optional, alternatively you can customize the frontmatter data.
-    ---@return table
-    note_frontmatter_func = function(note)
-      -- Add the title of the note as an alias.
-      if note.title then note:add_alias(note.title) end
-      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-      -- `note.metadata` contains any manually added fields in the frontmatter.
-      -- So here we just make sure those fields are kept in the frontmatter.
-      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-        for k, v in pairs(note.metadata) do
-          out[k] = v
+    -- customize the frontmatter data.
+    frontmatter = {
+      enabled = true,
+
+      ---@param note obsidian.Note
+      ---@return table<string, any>
+      func = function(note)
+        -- Add the title as an alias (like your old implementation)
+        if note.title then note:add_alias(note.title) end
+
+        -- Base frontmatter fields
+        local out = {
+          id = note.id,
+          aliases = note.aliases,
+          tags = note.tags,
+        }
+
+        -- Preserve any manually added metadata fields
+        if note.metadata and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
         end
-      end
-      return out
-    end,
+
+        return out
+      end,
+
+      -- Sort frontmatter fields (same behavior as your example)
+      sort = { "id", "aliases", "tags" },
+    },
   },
 }
